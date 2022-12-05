de1bd4c98fd49082e80e853a8d605aef8902526f
EYER0512 <eyepezruiz@gmail.com>
2022-12-05 18:25:57 -0500

    pruebita

---
+++ src/app/pages/actividad/materiales/form-modal-materiales/form-modal-materiales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-modal-materiales',
+  templateUrl: './form-modal-materiales.component.html',
+  styleUrls: ['./form-modal-materiales.component.css']
+})
+export class FormModalMaterialesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
