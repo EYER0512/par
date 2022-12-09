2adc2894bb8459ad8439440729e1ad0cd4799795
EYER0512 <eyepezruiz@gmail.com>
2022-12-09 10:36:40 -0500

    agregados

--- src/app/pages/actividad/actividad.module.ts
+++ src/app/pages/actividad/actividad.module.ts
@@ -4,7 +4,7 @@ import { MaterialesComponent } from './materiales/materiales.component';
 import { ProgramasComponent } from './programas/programas.component';
 import { TalleresComponent } from './talleres/talleres.component';
 import { ActividadComponent } from './actividad.component';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 
 import {ProgramaService} from "../../providers/services/programa.service";
 import {ActividadRoutingModule} from "./actividad-routing.module";
@@ -15,9 +15,14 @@ import { FormModalTallerComponent } from './talleres/form-modal-taller/form-moda
 import { CarrerasComponent } from './carreras/carreras.component';
 import { FacultadesComponent } from './facultades/facultades.component';
 import { TipoPersonaComponent } from './tipo-persona/tipo-persona.component';
-import {TipoPersonaService} from "../../providers/services/tipo-persona.service";
+import { TipoPersonaService } from "../../providers/services/tipo-persona.service";
 import { FormModalTipoPersonaComponent } from './tipo-persona/form-modal-tipo-persona/form-modal-tipo-persona.component';
 import { FormModalMaterialesComponent } from './materiales/form-modal-materiales/form-modal-materiales.component';
+import { PersonaComponent } from './persona/persona.component';
+import { FormModalPersonaComponent } from './persona/form-modal-persona/form-modal-persona.component';
+import { PersonaService } from 'src/app/providers/services/persona.service';
+import { CarrerasService } from 'src/app/providers/services/carreras.service';
+import { MaterialesService } from 'src/app/providers/services/materiales.service';
 
 export let validateAndFlattenComponentImports = undefined;
 
@@ -34,7 +39,9 @@ export let validateAndFlattenComponentImports = undefined;
     FacultadesComponent,
     TipoPersonaComponent,
     FormModalTipoPersonaComponent,
-    FormModalMaterialesComponent
+    FormModalMaterialesComponent,
+    PersonaComponent,
+    FormModalPersonaComponent
   ],
   imports: [
     CommonModule,
@@ -45,7 +52,10 @@ export let validateAndFlattenComponentImports = undefined;
   providers: [
     ProgramaService,
     TallerService,
-    TipoPersonaService
+    TipoPersonaService,
+    PersonaService,
+    CarrerasService,
+    MaterialesService
   ]
 })
 export class actividadmodule {
--- src/app/pages/actividad/materiales/form-modal-materiales/form-modal-materiales.component.ts
+++ src/app/pages/actividad/materiales/form-modal-materiales/form-modal-materiales.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialesService } from 'src/app/providers/services/materiales.service';
+import { TallerService } from 'src/app/providers/services/taller.service';
 
 @Component({
   selector: 'app-form-modal-materiales',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormModalMaterialesComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: any;
+  @Input() maId: any;
+  @Input() item: any;
+
+  frmMateriales!: FormGroup;
+  talleres: any = [];
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private materialesService: MaterialesService,
+    private tallerService: TallerService
+  ) { }
+
+  Id = document.getElementById('id');
 
   ngOnInit(): void {
+    this.formInit();
+    this.getTalleres();
+    if(this.item){
+      this.updateData();
+    }
+  }
+
+  getTalleres(): void {
+    this.tallerService.getAll$().subscribe(response => {
+      this.talleres = response.data || [];
+    });
+  }
+
+  formInit(): void {
+    const controls = {
+      taId: ['',[Validators.required]],
+      malink: ['',[Validators.required]],
+    }
+    this.frmMateriales= this.formBuilder.group(controls);
+  }
+
+  save(): void {
+    let data = Object.assign(this.frmMateriales.value, {talleres: {taId: this.frmMateriales.value.taId}});
+    this.tallerService.update$(this.maId, data).subscribe(response => {
+      if (response.success) {
+        this.activeModal.close({success: true, message:response.message});
+      }
+    });
+  }
+
+  update(): void{
+    let data = Object.assign(this.frmMateriales.value, {talleres: {taId: this.frmMateriales.value.taId}});
+    this.tallerService.update$(this.maId, data).subscribe(response => {
+      if (response.success) {
+        this.activeModal.close({success: true, message:response.message});
+      }
+    });
+  }
+
+  updateData(): void {
+    let data = Object.assign(this.frmMateriales.value, {talleres: {taId: this.frmMateriales.value.taId}});
+    this.frmMateriales.patchValue(data);
   }
 
 }
